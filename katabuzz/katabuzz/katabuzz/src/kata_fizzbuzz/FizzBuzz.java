39bc0442fcb2fae97a49a81ed097ad46f98b5fa6
TonyPerezTaboada <Tony.perez.taboada@gmail.com>
2017-04-05 13:55:23 +0200

    Primer commit colaborador

---
+++ katabuzz/katabuzz/katabuzz/src/kata_fizzbuzz/FizzBuzz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata_fizzbuzz;
+
+/**
+ 
+ * https://github.com/miguelcf/katabuzz/invitations
+ 
+ * @author dam119
+ */
+public class FizzBuzz {
+    
+    public int getFizzBuzz(int Num) {
+        
+        return Num;
+    }
+    
+}
